784b84a028ec61dc09f08414c00b9788cb0a7e4a
Chris Duell <me@chrisduell.com>
2013-01-25 00:36:56 +1100

    starting unittests

---
+++ tests/PocketConnectionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class PocketConnectionTest extends PHPUnit_Framework_TestCase
+{
+
+
+    public function setUp()
+    {
+
+    }
+
+    public function tearDown()
+    {
+
+    }
+
+
+    public function testShouldAlwaysPass()
+    {
+        $one = 1;
+        $this->assertEquals(1, $one);
+    }
+
+
+}
