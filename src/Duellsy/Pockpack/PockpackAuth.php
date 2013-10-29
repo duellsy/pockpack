32cf8c86fb49f728d929d4353abdfc336c6d9789
Dieter Van der Stock <dietervds@gmail.com>
2013-10-29 12:45:08 +0200

    Bundled the request code into one function to prevent duplication

--- src/Duellsy/Pockpack/PockpackAuth.php
+++ src/Duellsy/Pockpack/PockpackAuth.php
@@ -75,22 +75,9 @@ class PockpackAuth
     public function receiveToken($consumer_key, $request_token)
     {
 
-        $params = array(
-            'consumer_key'  => $consumer_key,
-            'code'  => $request_token
-        );
-
-        $client = new Client(self::BASE_URL);
-        $request = $client->post('/v3/oauth/authorize');
-        $request->getParams()->set('redirect.strict', true);
-        $request->setHeader('Content-Type', 'application/json; charset=UTF8');
-        $request->setHeader('X-Accept', 'application/json');
-        $request->setBody(json_encode($params));
-        $response = $request->send();
-
-        $data = json_decode($response->getBody());
-
-        $access_token = $data->access_token;
+        $pocketData = $this->convertRequestToken($consumer_key, $request_token);
+        
+        $access_token = $pocketData->access_token;
 
         return $access_token;
 
@@ -106,6 +93,23 @@ class PockpackAuth
     public function receiveTokenAndUsername($consumer_key, $request_token)
     {
 
+        $pocketData = $this->convertRequestToken($consumer_key, $request_token);
+
+        $returnData['access_token'] = $pocketData->access_token;
+        $returnData['username'] = $pocketData->username;
+        
+        return $returnData;
+    }
+
+    /**
+     * Convert a request token into a Pocket access token. 
+     * We also get the username in the same response from Pocket.
+     * 
+     * @param  string $consumer_key
+     * @param  string $request_token
+     */
+    private function convertRequestToken($consumer_key, $request_token)
+    {
         $params = array(
             'consumer_key'  => $consumer_key,
             'code'  => $request_token
@@ -120,12 +124,8 @@ class PockpackAuth
         $response = $request->send();
 
         $data = json_decode($response->getBody());
-        $returnData['access_token'] = $data->access_token;
-        $returnData['username'] = $data->username;
-        
 
-        return $returnData;
-
+        return $data;
     }
 
 
