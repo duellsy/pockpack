f07ad2726f422ef9989d484b41879de65b9b5428
Andrew Cairns <andrew@acairns.co.uk>
2013-11-06 16:28:36 +0000

    Forgot about the Mock plugin for Guzzle

--- src/Duellsy/Pockpack/Pockpack.php
+++ src/Duellsy/Pockpack/Pockpack.php
@@ -18,17 +18,18 @@ use Duellsy\Pockpack\NoPockpackQueueException;
  */
 class Pockpack
 {
-    const BASE_URL = 'https://getpocket.com';
 
-    private $consumer_key;
-    private $access_token;
-    private $client;
+    private $consumer_key = '';
+    private $access_token = '';
+
+    const BASE_URL = 'https://getpocket.com';
 
     public function __construct($consumer_key, $access_token)
     {
         $this->consumer_key = $consumer_key;
         $this->access_token = $access_token;
     }
+
 
     /**
      * Responsible for sending the request to the pocket API
@@ -47,7 +48,7 @@ class Pockpack
         $actions = json_encode($queue->getActions());
         $actions = urlencode($actions);
 
-        $client = $this->getClient();
+        $client = new Client(self::BASE_URL);
         $request = $client->get(
             '/v3/send?actions=' . $actions .
             '&consumer_key=' . $this->consumer_key .
@@ -62,6 +63,9 @@ class Pockpack
         return json_decode($response->getBody());
 
     }
+
+
+
 
     /**
      * Get a list of active bookmarks from the API
@@ -80,7 +84,7 @@ class Pockpack
         // combine the creds with any options sent
         $params = array_merge($params, $options);
 
-        $client = $this->getClient();
+        $client = new Client(self::BASE_URL);
         $request = $client->post('/v3/get');
         $request->getParams()->set('redirect.strict', true);
         $request->setHeader('Content-Type', 'application/json; charset=UTF8');
@@ -90,23 +94,6 @@ class Pockpack
 
         return json_decode($response->getBody());
 
-    }
-
-    /**
-     * Get the client used to query Pocket.
-     * Allows the client dependency to be injected at runtime for unit tests.
-     *
-     * @return Client HTTP Client used to communicate with Pocket
-     */
-    private function getClient()
-    {
-        if ( $this->client ) {
-            return $this->client;
-        }
-
-        $this->client = new Client(self::BASE_URL);
-
-        return $client;
     }
 
 }
