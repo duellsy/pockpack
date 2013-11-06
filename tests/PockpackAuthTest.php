1fbdd8e2ffec0f787965e8cd392955f67649bbaf
Andrew Cairns <andrew@acairns.co.uk>
2013-11-06 20:09:36 +0000

    Testing access tokens and returning username

--- tests/PockpackAuthTest.php
+++ tests/PockpackAuthTest.php
@@ -34,14 +34,44 @@ class PocketAuthTest extends PHPUnit_Framework_TestCase
     {
         $response = new Response(200);
         $response->setBody(json_encode(array(
-            'code' => 'fake_token'
+            'code' => 'fake_request_token'
         )));
 
         $this->setPocketResponse($response);
 
         $token = $this->pockpack_auth->connect('fake_consumer_key');
 
-        $this->assertEquals( 'fake_token', $token );
+        $this->assertEquals( 'fake_request_token', $token );
+    }
+
+    public function testReceivingAccessToken()
+    {
+        $response = new Response(200);
+        $response->setBody(json_encode(array(
+            'access_token' => 'fake_access_token'
+        )));
+
+        $this->setPocketResponse($response);
+
+        $token = $this->pockpack_auth->receiveToken('fake_consumer_key', 'fake_access_token');
+
+        $this->assertEquals( 'fake_access_token', $token );
+    }
+
+    public function testReceivingAccessTokenAndUsername()
+    {
+        $response = new Response(200);
+        $response->setBody(json_encode(array(
+            'access_token' => 'fake_access_token',
+            'username' => 'acairns'
+        )));
+
+        $this->setPocketResponse($response);
+
+        $data = $this->pockpack_auth->receiveTokenAndUsername('fake_consumer_key', 'fake_access_token');
+
+        $this->assertEquals( 'fake_access_token', $data['access_token'] );
+        $this->assertEquals( 'acairns', $data['username'] );
     }
 
     /**
